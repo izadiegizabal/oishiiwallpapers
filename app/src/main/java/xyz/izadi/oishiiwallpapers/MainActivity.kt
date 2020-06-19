7a89749dc721c5efd95378a1d0ba5f34c2a1c1c0
Izadi <64izadi@gmail.com>
2020-06-19 19:58:13 +0900

    feat: initial Android Studio setup

---
+++ app/src/main/java/xyz/izadi/oishiiwallpapers/MainActivity.kt
@@ -0,0 +1,11 @@
+package xyz.izadi.oishiiwallpapers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
